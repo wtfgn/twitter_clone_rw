8cddae406bedb9df98d8648f2705a22dbb61d63d
wtfgn <exal59@yahoo.com>
2024-01-10 14:15:48 +0800

    Added new libraries for file handling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     jwtAccessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
   },
 });
